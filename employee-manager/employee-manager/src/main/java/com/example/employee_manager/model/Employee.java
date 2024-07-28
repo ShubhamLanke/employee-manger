fce82155375d30ae816171b627ddebe0031b4c0c
shubham lanke <shubhamlanke111@gmail.com>
2024-07-28 17:06:34 +0530

    Employee model class

---
+++ employee-manager/employee-manager/src/main/java/com/example/employee_manager/model/Employee.java
@@ -0,0 +1,105 @@
+package com.example.employee_manager.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobRole;
+    private String mobileNumber;
+    private String imageUrl;
+
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+    public Employee(){}
+
+    public Employee(Long id, String name, String email,
+                    String jobRole, String mobileNumber,
+                    String imageUrl, String employeeCode) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.jobRole = jobRole;
+        this.mobileNumber = mobileNumber;
+        this.imageUrl = imageUrl;
+        this.employeeCode = employeeCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJobRole() {
+        return jobRole;
+    }
+
+    public void setJobRole(String jobRole) {
+        this.jobRole = jobRole;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", jobRole='" + jobRole + '\'' +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", employeeCode='" + employeeCode + '\'' +
+                '}';
+    }
+}
