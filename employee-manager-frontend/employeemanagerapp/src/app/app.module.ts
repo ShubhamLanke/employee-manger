415a9afa39c3f4b9a8a6fa8bdce130fb233f488d
shubham lanke <shubhamlanke111@gmail.com>
2024-08-06 22:34:07 +0530

    added some features

--- employee-manager-frontend/employeemanagerapp/src/app/app.module.ts
+++ employee-manager-frontend/employeemanagerapp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { EmployeeService } from './employee.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
