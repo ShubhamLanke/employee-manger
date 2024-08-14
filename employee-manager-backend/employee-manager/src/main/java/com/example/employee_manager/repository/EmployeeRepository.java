6ce2a408ce92def20df674c5e9dcccb2f64569d4
shubham lanke <shubhamlanke111@gmail.com>
2024-08-14 23:50:32 +0530

    update

--- employee-manager-backend/employee-manager/src/main/java/com/example/employee_manager/repository/EmployeeRepository.java
+++ employee-manager-backend/employee-manager/src/main/java/com/example/employee_manager/repository/EmployeeRepository.java
@@ -2,7 +2,6 @@ package com.example.employee_manager.repository;
 
 import com.example.employee_manager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
